d53c440afc26cf08bad9e5a53a0daddaa41780ef
dhritiguduru <dhritiguduru@gmail.com>
2023-02-24 11:32:01 -0600

    pendinganswercomponent

--- do-connect/src/app/pendinganswer/pendinganswer.component.ts
+++ do-connect/src/app/pendinganswer/pendinganswer.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Answer } from 'app/answer';
+import { AnswerService } from 'app/answer.service';
+
 
 @Component({
   selector: 'app-pendinganswer',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./pendinganswer.component.css']
 })
 export class PendinganswerComponent {
+  fAnsList =this.answerService.getunapprovedanswer();
+  answer:Answer=new Answer(0,0,"","",false,"","",[]);
+ 
+  tableData:any[]=[{}];
+
+  constructor(private answerService:AnswerService, private router:Router){}
+  onSubmit(){
+    this.fAnsList.subscribe(f=> {this.tableData= f as Answer[]});
+  }
+  goToHomePage(){
+    this.router.navigate(['/']);
+
+  }
+ 
+
 
 }
