a2c33d764b4d4e14dd40b33d708627d7d6e5c9f6
natewelty <natewelty1@gmail.com>
2023-02-23 21:05:58 -0600

    Cleaned up some junk

--- do-connect/src/app/app-routing.module.ts
+++ do-connect/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChatdashboardComponent } from './chatdashboard/chatdashboard.component';
 import { ChatroomComponent } from './chatroom/chatroom.component';
 import { CreateanswerComponent } from './createanswer/createanswer.component';
-import { DisplayquestionComponent } from './displayquestion/displayquestion.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NewquestionComponent } from './newquestion/newquestion.component';
@@ -11,40 +10,20 @@ import { PendinganswerComponent } from './pendinganswer/pendinganswer.component'
 import { PendingquestionComponent } from './pendingquestion/pendingquestion.component';
 import { SearchquestionComponent } from './searchquestion/searchquestion.component';
 
-const routes: Routes = [
-  {path: '',redirectTo: 'home', pathMatch: 'full'},
-  //login and registration
-  { path: 'home', component: HomeComponent },
+const routes: Routes = [{path: '',redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   {path: 'logincomponent', component: LoginComponent}, 
-  
-  //admin login or sign up
-  //
-  //
-  //
-  //
-  //add quesiton
   {path: 'newquestioncomponent', component: NewquestionComponent},
-  //serch question and display question
   {path: 'searchquestioncomponent', component: SearchquestionComponent},
-
-  // hit question -> create answer
   {path: 'creatanswercomponent', component: CreateanswerComponent},
-
-  //only admin able to see pending question and answers
   {path: 'pendingquestioncomponent', component: PendingquestionComponent},
   {path: 'pendinganswercomponent', component: PendinganswerComponent},
-
   {path: 'chatdashboard', component: ChatdashboardComponent},
-  {path: 'chatroom', component: ChatroomComponent},
-
+  {path: 'chatroom', component: ChatroomComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { 
-
-
-
-}
+export class AppRoutingModule { }
--- do-connect/src/app/pendingquestion/pendingquestion.component.ts
+++ do-connect/src/app/pendingquestion/pendingquestion.component.ts
@@ -7,18 +7,8 @@ import { Component } from '@angular/core';
 })
 export class PendingquestionComponent {
   
-  descrition_question:string;
-  image_src:string;
-  status:boolean ;
-  topic:string;
-  title:string;
-  constructor( descrition_question:string ="",image_src:string="",status:boolean =true,topic:string="",title:string="") {
+  constructor() {
  
-    this.descrition_question = descrition_question;
-    this.image_src = image_src;
-    this.status = status;
-    this.topic = topic;
-    this.title = title;
   }
 
 
